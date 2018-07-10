c663a524c51e41f472fa5dba856d388c07ad7647
naizicqc <910985309@qq.com>
2018-07-10 22:40:52 +0800

    debug:camera control

--- Assets/CameraController.cs
+++ Assets/CameraController.cs
@@ -27,28 +27,25 @@ public class CameraController : MonoBehaviour {
 	}
 
     // Update is called once per frame
-    void Update() {
+    void FixedUpdate() {
 
         Vector3 tempModelEuler = model.transform.eulerAngles; //记录模型旋转角
 
         //水平旋转
-        playerHandle.transform.Rotate(Vector3.up, pi.Camera_right * Time.deltaTime * horizontalSpeed);
+        playerHandle.transform.Rotate(Vector3.up, pi.Camera_right * Time.fixedDeltaTime * horizontalSpeed);
 
         //垂直旋转
-        tempEulerX -= pi.Camera_up * Time.deltaTime * verticalSpeed;
+        tempEulerX -= pi.Camera_up * Time.fixedDeltaTime * verticalSpeed;
         tempEulerX = Mathf.Clamp(tempEulerX, -40, 30);  //旋转角限制
         cameraHandle.transform.localEulerAngles = new Vector3(tempEulerX, 0, 0);
 
         model.transform.eulerAngles = tempModelEuler; //恢复模型旋转角
-    }
 
-    private void LateUpdate()
-    {
         //相机跟随 抖动太强，不做lerp和smoothDamp
         //camera.transform.position = Vector3.Lerp(camera.transform.position,transform.position,0.2f);
-        //camera.transform.position = Vector3.SmoothDamp(camera.transform.position, transform.position, ref cameraDampVelocity, cameraDampValue);
+        camera.transform.position = Vector3.SmoothDamp(camera.transform.position, transform.position, ref cameraDampVelocity, cameraDampValue);
         camera.transform.eulerAngles = transform.eulerAngles;
 
-        camera.transform.position = transform.position;
+        //camera.transform.position = transform.position;
     }
 }

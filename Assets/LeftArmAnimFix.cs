d2ea195a6c2acc22495c88a80d1d2b9b1b66e1ee
naizicqc <910985309@qq.com>
2018-07-17 19:56:50 +0800

    Add a Shield

---
+++ Assets/LeftArmAnimFix.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeftArmAnimFix : MonoBehaviour {
+
+    private void OnAnimatorIK()
+    {
+        print("IKIK");
+    }
+
+}

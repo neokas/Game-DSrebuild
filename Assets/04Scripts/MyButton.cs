e4ab85f208efe17fcb241383c614f2fd957caeab
naizicqc <910985309@qq.com>
2018-07-18 21:28:34 +0800

    Double trigger
    按键类 双击

--- Assets/04Scripts/MyButton.cs
+++ Assets/04Scripts/MyButton.cs
@@ -7,32 +7,56 @@ public class MyButton{
 	public bool isPressing = false;
     public bool onPressed = false;
     public bool onReleased = false;
+    public bool isExtending = false;
+
+    public float extendingDuration = 0.15f;
 
     private bool currentState = false;
     private bool lastState = false;
 
+    private MyTimer extTimer = new MyTimer();
+
     public void Tick(bool input)
     {
+
+        extTimer.Tick();
+
         currentState = input;
 
         isPressing = currentState;
 
         onPressed = false;
         onReleased = false;
-        if(currentState != lastState)
+        if (currentState != lastState)
         {
-            if(currentState == true)
+            if (currentState == true)
             {
                 onPressed = true;
             }
             else
             {
                 onReleased = true;
+                StartTimer(extTimer, extendingDuration);
             }
         }
 
         lastState = currentState;
 
+
+        if (extTimer.state == MyTimer.STATE.RUN)
+        {
+            isExtending = true;
+        }
+        else
+        {
+            isExtending = false;
+        }
+    }
+
+    private void StartTimer(MyTimer timer, float duration)
+    {
+        timer.duration = duration;
+        timer.Go();
     }
 
 }
